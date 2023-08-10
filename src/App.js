2d8eaffa1e12ce6df56e56cb6f00a05975ada37c
bintadam <zakiyaadan12@gmail.com>
2023-08-10 14:59:06 +0200

    created components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 function App() {
 return (
-  <div className="bg-custom-green-light h-screen">
-    <div className="p- border bg-teal-800">
-      <input className="border-0" placeholder="Add Items"/>
-      <button className="text-white bg-teal-500	">+</button>
-    </div>
+  <div className="bg-custom-green-light h-screen pt-24 p-96">
+      <div className="border-1 rounded bg-teal-800 w-2/3">
+        <input className="border-none focus:border-none focus:outline-none" placeholder="Add Items"/>
+        <button className="text-white bg-teal-500	">+</button>
+      </div>
   </div>
 )
 }

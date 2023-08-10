97db33793d9235433062685bf5854226483b50c0
bintadam <zakiyaadan12@gmail.com>
2023-08-10 19:45:41 +0200

    fixed error

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -28,7 +28,7 @@ function AddTodo({onAdd}){
             {isMobile && (
                 <div className="bg-custom-green-dark rounded border rounded-3xl">
                     <form className=" p-1 text-white" onSubmit={handleSubmit}>
-                        <input className="bg-custom-green-dark font-extralight pl-1" value={text} placeholder="Add Items" onChange={(e) => setText(e.target.value)}/>
+                        <input className="bg-custom-green-dark font-extralight pl-1 focus:outline-none" value={text} placeholder="Add Items" onChange={(e) => setText(e.target.value)}/>
                         <button type="submit" className="ml-60 bg-teal-400 rounded-full  w-8 h-8 text-base font-bold">+</button>
                     </form>
                 </div>

4f5dd7134e0ca02b0da1c500ba0955a7bedbeb83
bintadam <zakiyaadan12@gmail.com>
2023-08-10 19:07:42 +0200

    install react responsive

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,6 +1,11 @@
+import { queries } from "@testing-library/react";
 import { useState } from "react";
+import {useMediaQuery} from "react-responsive"
 
 function AddTodo({onAdd}){
+    const isDesktop = useMediaQuery({query: "(min-width : 1224px)"});
+    const isMobile = useMediaQuery({query: "(max-width: 1223px)"});
+
     const [text, setText] = useState('')
 
     const handleSubmit = (e) => {
@@ -11,12 +16,24 @@ function AddTodo({onAdd}){
     }
 
     return (
-        <div className="bg-custom-green-dark rounded border rounded-3xl">
-            <form className=" p-1 text-white" onSubmit={handleSubmit}>
-                <input className="bg-custom-green-dark font-extralight pl-1" value={text} placeholder="Add Items" onChange={(e) => setText(e.target.value)}/>
-                <button type="submit" className="ml-36 bg-teal-400 rounded-full  w-8 h-8 text-base font-bold">+</button>
-            </form>
-        </div>
+        <>
+            {isDesktop && (
+                <div className="bg-custom-green-dark rounded border rounded-3xl">
+                    <form className=" p-1 text-white" onSubmit={handleSubmit}>
+                        <input className="bg-custom-green-dark font-extralight pl-1" value={text} placeholder="Add Items" onChange={(e) => setText(e.target.value)}/>
+                        <button type="submit" className="ml-44 bg-teal-400 rounded-full  w-8 h-8 text-base font-bold">+</button>
+                    </form>
+                </div>
+            )}
+            {isMobile && (
+                <div className="bg-custom-green-dark rounded border rounded-3xl">
+                    <form className=" p-1 text-white" onSubmit={handleSubmit}>
+                        <input className="bg-custom-green-dark font-extralight pl-1" value={text} placeholder="Add Items" onChange={(e) => setText(e.target.value)}/>
+                        <button type="submit" className="ml-44 bg-teal-400 rounded-full  w-8 h-8 text-base font-bold">+</button>
+                    </form>
+                </div>
+            )}
+        </>
     )
 }
 
